49692298e3b513c9720c3c793c900071dffa07df
Janarthanan6119 <91175755+Janarthanan6119@users.noreply.github.com>
2025-07-28 09:40:04 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,27 @@
+#include"stdio.h"
+int main(){
+  //Loops - repeat the process until the specific is condition is met
+  //for,while,do-while
+  /*
+  while(cnd){
+  block of code
+  inc/dec;
+  }
+  
+ int i=0;
+ while(i<=5){//6
+  printf("%d",i);//5
+  i++;//6
+ }
+  */
+ //for - we must give the limit
+ //while - no need to give the limit
+ while(1){
+  int n;
+  scanf("%d",&n);//5 7 13 -1
+  if(n==-1){
+   break;
+  }
+
+ }
+}
